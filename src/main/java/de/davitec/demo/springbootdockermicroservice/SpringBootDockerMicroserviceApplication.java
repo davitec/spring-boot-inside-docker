331c30ff8178cdacda5228b7ff3b5510495790cd
Rico Schüppel <rs@davitec.de>
2018-03-09 12:40:05 +0100

    inital commit from spring initialzr

---
+++ src/main/java/de/davitec/demo/springbootdockermicroservice/SpringBootDockerMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package de.davitec.demo.springbootdockermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerMicroserviceApplication.class, args);
+	}
+}
---
+++ src/test/java/de/davitec/demo/springbootdockermicroservice/SpringBootDockerMicroserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package de.davitec.demo.springbootdockermicroservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootDockerMicroserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
