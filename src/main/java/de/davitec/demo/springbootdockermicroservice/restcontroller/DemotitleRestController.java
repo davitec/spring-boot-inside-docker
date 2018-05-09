75c73a42a7330dd54a0b00ec2c8bd18d0d71923b
Rico Schüppel <rs@davitec.de>
2018-05-09 09:20:25 +0200

    add ressource to get single item

--- src/main/java/de/davitec/demo/springbootdockermicroservice/restcontroller/DemotitleRestController.java
+++ src/main/java/de/davitec/demo/springbootdockermicroservice/restcontroller/DemotitleRestController.java
@@ -4,22 +4,26 @@ import de.davitec.demo.springbootdockermicroservice.models.DemoTitle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import de.davitec.demo.springbootdockermicroservice.repositories.DemotitleRepository;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "/api/demotitle")
 public class DemotitleRestController {
 
     @Autowired
     DemotitleRepository demotitleRepository;
 
-    @RequestMapping(value="/api/demotitle", method = RequestMethod.GET)
-    public ResponseEntity<?> getDemotitles(){
+    @GetMapping()
+    public ResponseEntity<?> getList(){
         List<DemoTitle> res = demotitleRepository.findAll();
         return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    @GetMapping(value="/{id}")
+    public ResponseEntity<?> getSingle(@PathVariable long id){
+        return new ResponseEntity<>(demotitleRepository.findOne(id), HttpStatus.OK);
     }
 }
